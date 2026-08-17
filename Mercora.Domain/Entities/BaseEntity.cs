7b82d528bad8de6605720c9050e9b5e1f2fff981
Shah Jalal Dip <shahjalaldip246@gmail.com>
2026-08-18 01:56:35 +0600

    Add User and Product Entity

--- Mercora.Domain/Entities/BaseEntity.cs
+++ Mercora.Domain/Entities/BaseEntity.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mercora.Domain.Entities
 {
-    internal class BaseEntity
+    public class BaseEntity
     {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime? DeletedAt { get; set; }
     }
 }
